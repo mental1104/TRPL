fa25f3a921e64e30251de47f72e6443a13b97092
mental1104 <mental1104@gmail.com>
2023-10-20 23:06:41 +0800

    Hello World in Rust

---
+++ main.rs
@@ -0,0 +1,9 @@
+/*
+ * @Date: 2023-10-20 22:51:39
+ * @Author: mental1104 mental1104@gmail.com
+ * @LastEditors: mental1104 mental1104@gmail.com
+ * @LastEditTime: 2023-10-20 22:51:48
+ */
+fn main() {
+    println!("Hello world!");
+}
